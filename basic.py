5437049afc003d5ee0815db4ee5230dbd454e689
Naeem Ahmed <naeemahmedbdn2002@gmail.com>
2026-02-09 18:57:07 +0500

    Add basic Python variable and list operations

---
+++ basic.py
@@ -0,0 +1,74 @@
+# # Variables
+# teacher_name = "Naeem"
+
+# print(teacher_name)
+# print(id(teacher_name))
+
+# # Variable => vary + able
+
+# teacher_name = "Faheem"
+
+# print(teacher_name)
+
+# print(id(teacher_name))
+
+# brothers = 4
+# firt_id = id(brothers)
+# print(id(brothers))
+
+# brothers = 6
+# sec_id = id(brothers)
+# print(id(brothers))
+
+# print(firt_id==sec_id)
+
+# height = 5.5 # float
+# print(height)
+
+# # Data types
+# num = 6
+# height = 5.5 # Float
+# name = "Faheem"  # String
+# graduate = False   # Boolean
+
+# print(type(num))
+# print(type(height))
+# print(type(name))
+# print(type(graduate))
+
+# print(40 + 60)
+
+math = 40
+engl = 60
+
+result = math + engl
+
+print(result)
+
+per = (result/200)*100
+
+print(per)
+
+
+# brothers_inf = "Naeem, Faheem, Mujeeb, Abubakar, Najeebullah"
+
+brothers_info = ["Naeem", "Faheem", "Mujeeb", "Abubakar", "Najeebullah"]
+
+# print(brothers_info[0:3])
+
+# print(brothers_info[-1])
+
+brothers_info.append("Nadeem")
+
+print(brothers_info)
+
+brothers_info.insert(1, "Ayaz")
+
+print(brothers_info)
+
+brothers_info.remove("Faheem")
+
+print(brothers_info)
+
+personal_infor = ["Naeem", "Usman", 23, "Badin", "Graduate", 3.42]
+print(personal_infor)
